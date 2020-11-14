b245102908614ba6f465a3fc7883525824ba389c
Muhammad Kamal <muhammedkamal022@gmail.com>
2020-11-14 19:15:09 +0200

    Hala solved the error :)

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -1,4 +1,6 @@
-
+<?php
+    use App\Models\User;
+?>
 @extends('layouts.app')
 
 @section('content')
@@ -7,6 +9,7 @@
 <div class="card">
     <ul class="list-group-flush">
         @foreach($products as $post)
+
         <div class="row">
             <div class="col-md-4">
                 <img style="width:100%" src="/Buily/public/storage/coverimages/{{$post->cover}}" alt="Post Cover image">
@@ -15,7 +18,7 @@
                 <h3><a href="products/{{$post->id}}">{{$post->name}}</a></h3>
                 <h4>Price: {{$post->price}}</h4>
                 <h4>On Stock: {{$post->quantity}}</h4>
-                <small>Added at {{$post->created_at}}|by {{User::find($post->user_id)->name}}</small>
+                <small>Added at {{$post->created_at}} | by {{User::find($post->user_id)->name}}</small>
             </div>
         </div>
         @endforeach
